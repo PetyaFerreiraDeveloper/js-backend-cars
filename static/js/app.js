cf1d2e7b5fbf2c6636eb2b49a4bfa5a46a29ff4a
Petya <petianaidenova@gmail.com>
2022-06-11 22:02:29 +0200

    create show more functionallity

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,12 @@
-alert('it works')
+document.getElementById('cars').addEventListener('click', (e) => {
+    if(e.target.classList.contains('more')) {
+        const desc = e.target.parentElement.querySelector('.description');
+        if (desc.style.display == 'block') {
+            desc.style.display = 'none';
+            e.target.textContent = 'Show More';
+        } else {
+            desc.style.display = 'block';
+            e.target.textContent = 'Hide';
+        }
+    }
+})
